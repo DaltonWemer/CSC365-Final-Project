205cd8fd2e3e23b532991d6dcfefd418d0dcb8b7
Brendan Beck <brendan2040@live.missouristate.edu>
2020-04-20 01:55:42 -0500

    Final homework 4 after war creation

--- src/test/java/edu/missouristate/BrenbeckDaltwemFinalprojectApplicationTests.java
+++ src/test/java/edu/missouristate/BrenbeckDaltwemFinalprojectApplicationTests.java
@@ -3,7 +3,9 @@ package edu.missouristate;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import edu.missouristate.config.BrenbeckDaltwemFinalprojectApplication;
+
+@SpringBootTest(classes=BrenbeckDaltwemFinalprojectApplication.class)
 class BrenbeckDaltwemFinalprojectApplicationTests {
 
 	@Test
